cbc99ae87ab3f9494f3318ad549c75d108083882
YulDmi <barsetca@rambler.ru>
2020-03-26 07:02:01 +0300

    Урок1

---
+++ src/lesson01/Apple.java
@@ -0,0 +1,11 @@
+package lesson01;
+
+public class Apple extends Fruits {
+    public Apple() {
+        super( 1.0f);
+    }
+
+    public Apple(float weight) {
+        super(weight);
+    }
+}
---
+++ src/lesson01/Orange.java
@@ -0,0 +1,12 @@
+package lesson01;
+
+
+public class Orange extends Fruits {
+    public Orange() {
+        super(1.5f);
+    }
+
+    public Orange(float weight) {
+        super(weight);
+    }
+}
